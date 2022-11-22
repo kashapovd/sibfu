a4ea26294eadc3e7a6521550194179907b6b5dec
Yaroslav <55892644+kashapovd@users.noreply.github.com>
2022-11-22 22:03:31 +0700

    [hcs][work1] it works!

--- hcs/work1/array-add.cu
+++ hcs/work1/array-add.cu
@@ -1,4 +1,6 @@
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
+#include <helper_cuda.h>
 #include "tools.cpp"
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,14 +10,13 @@
 #include <cstring>
 #include <cassert>
 
-
-#define ARRAY_SIZE 100000
+#define INIT_ARRAY_SIZE 100
 #define THREADS_PER_BLOCKS 1024
 
 #define timer_start (std::chrono::high_resolution_clock::now())
 #if defined timer_start
     #define timer_elapsed(t) (std::chrono::duration_cast \
-                                <std::chrono::milliseconds> \
+                                <std::chrono::nanoseconds> \
                                 (std::chrono::high_resolution_clock \
                                         ::now() - t).count())
 #endif
@@ -26,63 +27,85 @@ void vector_add_cpu(int32_t *host_a, int32_t *host_b, int32_t *host_c, uint64_t 
         host_c[i] = host_a[i] + host_b[i];
 }
 
-
 __global__ void vector_add_gpu(int32_t *device_a, int32_t *device_b, int32_t *device_c, uint64_t n) {
     int i =  blockDim.x * blockIdx.x + threadIdx.x;
     if (i < n)
         device_c[i] = device_a[i] + device_b[i];
 }
 
-int main() {
+void HostVsDevice(size_t size, std::ostream &ost) {
+    auto device_blocks = (size + THREADS_PER_BLOCKS - 1) / THREADS_PER_BLOCKS;
 
-    int32_t *host_a, *host_b, *host_c;
+    int32_t *host_a, *host_b, *host_c, *buff_device_on_host;
     int32_t *device_a, *device_b, *device_c;
 
-    // malloc mem on host
-    host_a = (int32_t *)malloc(ARRAY_SIZE * sizeof(int32_t));
-    host_b = (int32_t *)malloc(ARRAY_SIZE * sizeof(int32_t));
-    host_c = (int32_t *)malloc(ARRAY_SIZE * sizeof(int32_t));
+    // alloc mem on host
+    checkCudaErrors(cudaMallocHost(&buff_device_on_host, size * sizeof(int32_t)));
+    checkCudaErrors(cudaMallocHost(&host_a, size * sizeof(int32_t)));
+    checkCudaErrors(cudaMallocHost(&host_b, size * sizeof(int32_t)));
+    checkCudaErrors(cudaMallocHost(&host_c, size * sizeof(int32_t)));
 
-    // malloc mem on device
-    auto r = cudaMalloc(&device_a, ARRAY_SIZE * sizeof(int32_t));
-    assert((r == cudaError::cudaSuccess));
-    r = cudaMalloc(&device_a, ARRAY_SIZE * sizeof(int32_t));
-    assert((r == cudaError::cudaSuccess));
-    r = cudaMalloc(&device_a, ARRAY_SIZE * sizeof(int32_t));
-    assert((r == cudaError::cudaSuccess));
+    // alloc mem on device
+    checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&device_a), size * sizeof(int32_t)));
+    checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&device_b), size * sizeof(int32_t)));
+    checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&device_c), size * sizeof(int32_t)));
 
-    fill_random(host_a, ARRAY_SIZE);
-    fill_random(host_b, ARRAY_SIZE);
+    // create cuda events to measure timing 
+    cudaEvent_t start, stop;
+    checkCudaErrors(cudaEventCreate(&start));
+    checkCudaErrors(cudaEventCreate(&stop));
 
-    cudaMemcpy(device_a, host_a, ARRAY_SIZE * sizeof(int32_t), cudaMemcpyHostToDevice);
-    cudaMemcpy(device_b, host_b, ARRAY_SIZE * sizeof(int32_t), cudaMemcpyHostToDevice);
+    // init host memory
+    fill_random(host_a, size);
+    fill_random(host_b, size);
 
+    checkCudaErrors(cudaMemcpy(device_a, host_a, size * sizeof(int32_t), cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(device_b, host_b, size * sizeof(int32_t), cudaMemcpyHostToDevice));
+
+    // exec calc on host
     auto timer = timer_start;
-    vector_add_cpu(host_a, host_b, host_c, ARRAY_SIZE);
+    vector_add_cpu(host_a, host_b, host_c, size);
     auto elapsed = timer_elapsed(timer);
-    std::cout << "vector_add_cpu_int: " << elapsed << " seconds" << std::endl;
+    
+    ost << "Host calcs took: " << float(elapsed/1000000.0) << "ms" << std::endl;
 
-    auto blocks = (ARRAY_SIZE + THREADS_PER_BLOCKS - 1) / THREADS_PER_BLOCKS;
-    timer = timer_start;
-    vector_add_gpu <<<blocks, THREADS_PER_BLOCKS>>> (device_a, device_b, device_c, ARRAY_SIZE);
-    cudaDeviceSynchronize();
-    elapsed = timer_elapsed(timer);
-    std::cout << "vector_add_gpu_int: " << elapsed << " seconds" << std::endl;
+    checkCudaErrors(cudaProfilerStart());
+    // exec calc on device
+    checkCudaErrors(cudaEventRecord(start, 0));
+    vector_add_gpu <<<device_blocks, THREADS_PER_BLOCKS>>> (device_a, device_b, device_c, size);
+    checkCudaErrors(cudaEventRecord(stop, 0));
+    checkCudaErrors(cudaEventSynchronize(stop));
+    checkCudaErrors(cudaProfilerStop());
 
-    auto buff_device_on_host = (int32_t *)malloc(ARRAY_SIZE * sizeof(int32_t));
-    cudaMemcpy(buff_device_on_host, device_c, ARRAY_SIZE * sizeof(int32_t), cudaMemcpyDeviceToHost);
-    if (compare_array(host_c, buff_device_on_host, ARRAY_SIZE))
-        std::cout << "Computed arrays from device and host are identical" << std::endl;
+    float msecTotal = 0.0f;
+    cudaEventElapsedTime(&msecTotal, start, stop);
+    ost << "Device calcs took: " << msecTotal << "ms" << std::endl; 
+
+    checkCudaErrors(cudaMemcpy(buff_device_on_host, device_c, size * sizeof(int32_t), cudaMemcpyDeviceToHost));
+
+    if (compare_array(host_c, buff_device_on_host, size))
+        ost << "Computed arrays from device and host are identical" << std::endl;
     else 
-        std::cout << "Computed arrays from device and host aren't identical" << std::endl;
+        ost << "Computed arrays from device and host aren't identical" << std::endl;
 
-    cudaFree(device_a);
-    cudaFree(device_b);
-    cudaFree(device_b);
+    checkCudaErrors(cudaFree(device_a));
+    checkCudaErrors(cudaFree(device_b));
+    checkCudaErrors(cudaFree(device_c));
 
-    free(host_a);
-    free(host_b);
-    free(host_c);
+    checkCudaErrors(cudaFreeHost(buff_device_on_host));
+    checkCudaErrors(cudaFreeHost(host_a));
+    checkCudaErrors(cudaFreeHost(host_b));
+    checkCudaErrors(cudaFreeHost(host_c));
 
+}
+
+int main(int argc, char **argv) {
+    int devID = findCudaDevice(argc, (const char **)argv);
+    
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "Test №" << i << " === Array size: " << INIT_ARRAY_SIZE*std::pow(10, i) << std::endl; 
+        HostVsDevice(INIT_ARRAY_SIZE*std::pow(10, i), std::cout);
+        std::cout << std::endl;
+    }
     return 0;
 }
